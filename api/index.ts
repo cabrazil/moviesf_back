d5f52bb258b557022ef98103e93392adb222c8fe
cabrazil <cabrazil@gmail.com>
2025-09-11 21:44:10 -0300

    index.ts adjusted

--- api/index.ts
+++ api/index.ts
@@ -25,7 +25,13 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: [
+    'Content-Type', 
+    'Authorization', 
+    'X-Requested-With',
+    'x-csrf-token',
+    'X-CSRF-Token'
+  ]
 }));
 app.use(express.json());
 
